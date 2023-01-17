cb79b2110b3996e813df88ff4d19b481e407c870
murat erkmen <murat.erkmen@asendia.com>
2023-01-17 22:30:55 +0300

    leetcode Q.228 solved

--- Arkensoft.Exercise/Arkensoft.Exercise/Program.cs
+++ Arkensoft.Exercise/Arkensoft.Exercise/Program.cs
@@ -1,10 +1,18 @@
 ﻿
 using Arkensoft.Exercise;
+using System.Linq;
 
 FrequencyQueries frequency = new FrequencyQueries();
 
+SummaryRanges ranges = new SummaryRanges();
+var summary = ranges.GetSummaryRanges(new int[] { 0, 1, 2, 4, 5, 7 });
+
+Console.WriteLine("[0, 1, 2, 4, 5, 7] --> " + string.Join( ',', summary));
+
+ summary = ranges.GetSummaryRanges(new int[] { 0, 2, 3, 4, 6, 8, 9 });
+
+Console.WriteLine("[0, 2, 3, 4, 6, 8, 9] --> " + string.Join(',', summary));
 
 
 Console.WriteLine("Hello, World!");
-
 
---
+++ Arkensoft.Exercise/Arkensoft.Exercise/SummaryRanges.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkensoft.Exercise
+{
+    /// <summary>
+    /// https://leetcode.com/problems/summary-ranges/
+    /// Q.228
+    /// You are given a sorted unique integer array nums.A range[a, b] is the set of all integers from a to b(inclusive).
+    /// Return the smallest sorted list of ranges that cover all the numbers in the array exactly.That is, each element of nums is covered by exactly one of the ranges, 
+    /// and there is no integer x such that x is in one of the ranges but not in nums.
+    /// </summary>
+    public class SummaryRanges
+    {
+        public List<string> GetSummaryRanges(int[] nums)
+        {
+            List<string> response = new List<string>();
+
+            int a = 0;
+            int b = 0;
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                a = nums[i];
+                while (nums.Length - 1 > i && nums[i] + 1 == nums[i + 1])
+                {
+                    i++;
+                }
+
+                b = nums[i];
+
+                if (a == b)
+                {
+                    response.Add(a.ToString());
+                }
+                else
+                {
+                    response.Add(a + "->" + b);
+                }
+            }
+
+            return response;
+        }
+    }
+}
